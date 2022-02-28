978a60293bee36df7e8e7ac959fe69b63b1730d9
Laxmi <ldh001@uib.no>
2022-02-28 12:52:14 +0100

    Gjort Coordinate.java på 01-grid.md

--- src/main/java/inf101v22/grid/Coordinate.java
+++ src/main/java/inf101v22/grid/Coordinate.java
@@ -1,5 +1,68 @@
 package inf101v22.grid;
 
-public class Coordinate {
+import java.util.Iterator;
+import java.util.Objects;
+
+public class Coordinate implements IGrid{
+    int row;
+    int col;
+
+
+    @Override
+    public final int getRows() {
+        return row;
+    }
+
+    @Override
+    public final int getCols() {
+        return col;
+    }
+
+    @Override
+    public void set(Coordinate coordinate, Object value) {
+
+    }
+
+    @Override
+    public Object get(Coordinate coordinate) {
+        return null;
+    }
+
+    @Override
+    public boolean coordinateIsOnGrid(Coordinate coordinate) {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
     // TODO: create this class
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return row == that.row && col == that.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
+    }
+
+    @Override
+    public boolean equals() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinate{" +
+                "row=" + row +
+                ", col=" + col +
+                '}';
+    }
 }
